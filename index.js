7145e2263fe0e7bdf305734e33086705b07657ed
cauaramiresif <cauaramires.bg003@academico.ifsul.edu.br>
2026-03-17 09:38:29 -0300

    Adicionei um txt

---
+++ index.js
@@ -0,0 +1,51 @@
+console.log("O JSON VOLTOU :// ....")
+
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    res.send ('Bem-vindo ao universo dos heróis da Marvel e da DC')
+})
+
+app.get('/marvel', (req, res) => {
+    res.send ('Bem-vindo ao universo dos heróis da Marvel')
+})
+
+app.get('/dc', (req, res) => {
+    res.send ('Bem-vindo ao universo dos heróis da DC')
+})
+
+app.get('/heroi/:personagem', (req, res) => {
+    const personagem = req.params.personagem
+    if(req.params.personagem == "hulk"){
+        res.send (req.params.personagem + ' é um herói da Marvel')
+    }
+    else if(req.params.personagem == "lexluthor"){
+        res.send (req.params.personagem + ' é um vilão da DC')
+    }
+    else if(req.params.personagem == "homemdeferro"){
+        res.send (req.params.personagem + ' é um herói da Marvel')
+    }
+    else if(req.params.personagem == "coringa"){
+        res.send (req.params.personagem + ' é um vilão da DC')
+    }
+    else if(req.params.personagem == "superman"){
+        res.send (req.params.personagem + ' é um herói da DC')
+    }
+    else if(req.params.personagem == "thanos"){
+        res.send (req.params.personagem + ' é um vilão da Marvel')
+    }
+    else if(req.params.personagem == "batman"){
+        res.send (req.params.personagem + ' é um herói da DC')
+    }
+    else if(req.params.personagem == "magneto"){
+        res.send (req.params.personagem + ' é um vilão da Marvel')
+    }
+    else
+    res.send ('Personagem: '+req.params.personagem)
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
